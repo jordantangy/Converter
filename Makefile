74dea02275fe3e1140d0681f4dd056939c62b177
jordantangy@gmail.com <jordantangy@gmail.com>
2020-10-04 19:57:52 +0300

    update

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@
 all :
 	clang++-9 -std=c++2a converter.cpp 
 	./a.out
-	rm a.out
+
 
 	
--- converter.cpp
+++ converter.cpp
@@ -22,6 +22,7 @@ while(!(s.empty())){
     st += to_string(a);
 }
 cout << st << endl;
+cout << " " << endl;
 
 }
 
@@ -50,17 +51,23 @@ void toDecimal(long num){
     }
 
 cout << ans << endl;
+cout << " " << endl;
     
 }
 
 
 
+
 int main(){
 
+    
+    while(1){
     int choice;
-    cout << "Decimal to Binary ---> Press 1" << endl;
-    cout << "Binary to Decimal ---> Press 2" << endl;
+    cout << "To convert a number from Decimal to Binary ---> Press 1" << endl;
+    cout << "To convert a number from Binary to Decimal ---> Press 2" << endl;
+    cout << "To exit the program, press Ctrl+C" << endl;
     cin >> choice;
+ 
     if (choice == 1){
     
     long num;
@@ -70,13 +77,49 @@ int main(){
     toBinary(num);
 
     }
-    else if(choice == 2){
+
+        else  if(choice == 2){
         long num2;
         cout << "Enter a binary number to convert to decimal" << endl;
         cin>>num2;
+        int flag = 1;
+        while(flag){
+
+            try{
+            string s = to_string(num2);
+            bool b = 1;
+            for (size_t i = 0; i < s.length(); i++)
+            {
+                if(s[i] == '0' || s[i] == '1'){
+                    continue;
+                }
+                else{
+                    b = 0;
+                    break;
+                }
+            }
+
+            if(b == 0){
+                throw _ERRNO_H;
+            }
+           else{
+               break;
+           }
+
+        }
+        catch (int err){
+            cout << "The number you entered is not a binary number" << endl;
+            cout << "Please, enter a binary number:" << endl;
+            cin >>num2;
+        }
+
+    }
         cout << "The binary number " << num2 << " in decimal is : " ;
         toDecimal(num2);
-    }
     
+
+    }
+
+  }
     
 }
